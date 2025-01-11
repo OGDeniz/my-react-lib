f4b8d48ca63893ae8284674ac6835ca9d6a4911c
OGDeniz <118958904+OGDeniz@users.noreply.github.com>
2025-01-11 09:52:10 +0100

    Aktualisiere Button-Seite zur Verwendung von React 18; integriere createRoot für das Rendering

--- src/pages/buttons/index.tsx
+++ src/pages/buttons/index.tsx
@@ -1,12 +1,15 @@
-import ButtonEmphasis from '../../components/UI/Buttons/ButtonEphasis';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ButtonEmphasis from '../../components/UI/Buttons/ButtonEphasis';
 
-
-const Buttons: React.FC = () => {
-    return (
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <React.StrictMode>
         <BrowserRouter>
         <ButtonEmphasis disabled={false} appBarColor="primary" />
         </BrowserRouter>
-    );
-    };
-    
+    </React.StrictMode>
+);
+ 
